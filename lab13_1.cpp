283a2b4dcff425b4487589dfde9bcd3990acf81d
Krittaphat <krittaphat_su@cmu.ac.th>
2024-12-27 15:51:42 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,33 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum1 , sum2 , sum3 = 1 , sum4 ;
+    double max = 0 , min = 20 ;
+    for (int i = 0 ; i < N ; i ++){
+        sum1 += A[i] ;
+        sum3 *= A[i] ;
+        sum4 += 1 / A[i] ;
+    }
+    B[0] = sum1 / N ;
+
+    for (int j = 0 ; j < N ; j ++){
+        if (A[j] > max){
+            max = A[j] ;
+        }
+        if (A[j] < min){
+            min = A[j] ;
+        }
+    }
+    
+    for (int k = 0 ; k < N ; k ++){
+        sum2 += pow(A[k] - B[0] , 2) / N ;
+    }
+    
+    B[1] = sqrt(sum2) ;
+    B[2] = pow (sum3 , 1.0/N) ;
+    B[3] = N / sum4 ;
+    B[4] = max ;
+    B[5] = min ;
+}
